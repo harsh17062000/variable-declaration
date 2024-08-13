e255153e1cdc05c37b02177cdc2609ec41339ab5
harsh17062000 <harshgupt100@gmail.com>
2024-08-13 13:42:22 +0530

    variable decalaration

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int roll_no = 12;
+    int total_marks = 199;
+    cout << "roll number = " << roll_no << " \n ";
+    cout << "total marks = " << total_marks << " \n ";
+
+    return 0;
+
+}
